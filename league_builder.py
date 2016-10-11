36cc6b8cd04bb7f576fa99b3fedfa84043b6d6ab
Travis <tjbailey10@gmail.com>
2016-10-11 11:20:42 -0400

    Added ability to generate letters

--- league_builder.py
+++ league_builder.py
@@ -6,51 +6,59 @@ def open_csv(file_name):
 		player_dict = list(csv.DictReader(file, delimiter=','))
 	return player_dict
 
+def assigner(teams, players):
+	count = 1
+	for player in players:
+		if count == 1:
+			player["Team"] = teams[0]
+			count += 1
+		elif count == 2:
+			player["Team"] = teams[1]
+			count += 1
+		else:
+			player["Team"] = teams[2]
+			count = 1
+
+
 
 def build_teams(player_dict):
 	experienced_players = []
 	noobs = []
+	teams = ["Sharks", "Dragons", "Raptors"]
 	for player in player_dict:
 		if player["Soccer Experience"] == "YES":
 			experienced_players.append(player)
 		else:
 			noobs.append(player)
-	count = 1
-	for player in experienced_players:
-		if count == 1:
-			player["Team"] = "Sharks"
-			count += 1
-		elif count == 2:
-			player["Team"] = "Dragons"
-			count += 1
-		else:
-			player["Team"] = "Raptors"
-			count = 1
-	count_2 = 1
-	for player in noobs:
-		if count_2 == 1:
-			player["Team"] = "Sharks"
-			count_2 += 1
-		elif count_2 == 2:
-			player["Team"] = "Dragons"
-			count_2 += 1
-		else:
-			player["Team"] = "Raptors"
-			count_2 = 1
+	assigner(teams, experienced_players)
+	assigner(teams, noobs)
+
 	return experienced_players, noobs
 
 
-def generate_letters(players_experienced, players_new):
-	pass
+def generate_letters(players):
+	for player in players:
+		name_split = player["Name"].lower().split()
+		name_join = "_".join(name_split)
+		file = open("{}.txt".format(name_join), "w")
+		practice_time = ""
+		if player["Team"] == "Raptors":
+			practice_time = "March 18, 1PM"
+		elif player["Team"] == "Sharks":
+			practice_time = "March 17, 3PM"
+		else:
+			practice_time = "March 17, 1PM"
+		file.write("""
+Dear {Guardian Name(s)},
+Your child has been selected to participate in our Quidditch league! {Name} will be playing on the {Team} team. The team's first practice will be on {Practice}.
+Please refer to our sport as 'Soccer' in public to avoid issues with muggles. Thank you.
+Yours sincerely,
+	Travis
+			""".format(Practice = practice_time, **player))
 
 		
 if __name__ == '__main__':
 	players = open_csv('soccer_players.csv')
 	players_experienced, players_new = build_teams(players)
-
-# Break the players into two different lists depending if they have previous experience
-
-
-# Reference a list of teams and assign players to teams.
-
-
+	generate_letters(players_experienced)
+	generate_letters(players_new)
